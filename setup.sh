4d84b3d4fa87899199a093a41bc0c1b4c6c5b8eb
netkiller <netkiller@msn.com>
2015-11-04 17:50:32 +0800

    cconv

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 ln -s /opt/workspace/netkiller.github.com /home/neo/public_html
 ln -s /opt/workspace /home/neo/workspace
 
-sudo apt-get install -y make xsltproc docbook-xsl docbook-website
+sudo apt-get install -y make cconv xsltproc docbook-xsl docbook-website
 
